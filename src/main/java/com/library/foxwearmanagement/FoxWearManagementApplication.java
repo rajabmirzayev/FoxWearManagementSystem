a09ec7e2f3fa8b1120da5b30739ef5e13db21262
rajabmirzayev <rajabmirzayevv@gmail.com>
2026-02-08 21:39:39 +0400

    initial commit

---
+++ src/main/java/com/library/foxwearmanagement/FoxWearManagementApplication.java
@@ -0,0 +1,13 @@
+package com.library.foxwearmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxWearManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoxWearManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/library/foxwearmanagement/FoxWearManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.foxwearmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoxWearManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
