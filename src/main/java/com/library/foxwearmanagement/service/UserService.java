4232ad382b9739ea02e2d59e533811d3c9975d5a
rajabmirzayev <rajabmirzayevv@gmail.com>
2026-02-09 16:50:59 +0400

    refactor: deleted unused elements

--- src/main/java/com/library/foxwearmanagement/service/UserService.java
+++ src/main/java/com/library/foxwearmanagement/service/UserService.java
@@ -8,9 +8,7 @@ import com.library.foxwearmanagement.exception.PhoneNumberAlreadyExistException;
 import com.library.foxwearmanagement.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
